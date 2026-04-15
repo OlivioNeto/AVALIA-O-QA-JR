b6dd340f56e3b10cb025f741c42b9c5a71bcec9e
Olivio Neto <netoolivio34@gmail.com>
2026-04-15 12:46:27 -0300

    Parte do JSON realizada, consumindo API

--- PROJETO QA/Form1.cs
+++ PROJETO QA/Form1.cs
@@ -1,4 +1,6 @@
 using System.Configuration;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace PROJETO_QA
 {
@@ -33,8 +35,14 @@ namespace PROJETO_QA
         private async Task<double> ObterPrecoBitcoin() // async pois o método usa await, algo de de fora e Task double para devolver double
         {
             HttpClient clientHttp = new HttpClient(); // variavel para requisição
-            var respostaHttp = await clientHttp.GetAsync("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl");
-            await respostaHttp.Content.ReadAsStringAsync();
+            
+            var respostaHttp = await clientHttp.GetAsync("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl"); // pegando uma resposta da API
+           
+            string guardandoJson = await respostaHttp.Content.ReadAsStringAsync(); // pegando um json e fazendo ele ser lido como string
+
+            var objDesserializado = JsonSerializer.Deserialize<RespostaBitcoin>(guardandoJson); // tranformando o json em algpo que o C# entenda
+            
+            return objDesserializado.bitcoin.brl; // retornando o objeto com a moeda bitcoin e o brl que é a moeda brasileira
         }
 
         class RespostaBitcoin // é um objeto com a prioridade bitcoin
--- PROJETO QA/obj/Debug/net6.0-windows/PROJETO QA.AssemblyInfo.cs
+++ PROJETO QA/obj/Debug/net6.0-windows/PROJETO QA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJETO QA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12718682867bc6258c6d93a24a3b5f1cd0948eb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e23e5b005eae1153525dcaaab470525d6311d34")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJETO QA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJETO QA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
